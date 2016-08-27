9e9e76f9107186a165c88856ef5b75a8c36103bf
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-08-28 01:50:26 +0800

    Add print method for Output class

--- src/com/teamfour/implicitinvocations/Output.java
+++ src/com/teamfour/implicitinvocations/Output.java
@@ -2,8 +2,11 @@ package com.teamfour.implicitinvocations;
 
 public class Output {
 
-    public Output() {
-        // TODO Auto-generated constructor stub
+    public static void print(LineStorage lineStorage){
+        lineStorage.sort();
+        String line;
+        while((line = lineStorage.getLine()) != null){
+            System.out.println(line);
+        }
     }
-
 }
