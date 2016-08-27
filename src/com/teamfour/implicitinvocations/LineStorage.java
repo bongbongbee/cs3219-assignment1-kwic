f0c37a350fb98a1faa2c48f4fac2461bdedf5f26
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-08-27 18:02:08 +0800

    Add observable methods for LineStorage class

--- src/com/teamfour/implicitinvocations/LineStorage.java
+++ src/com/teamfour/implicitinvocations/LineStorage.java
@@ -2,10 +2,30 @@ package com.teamfour.implicitinvocations;
 
 import java.util.ArrayList;
 import java.util.Observable;
+import java.util.Observer;
 
 public class LineStorage extends Observable {
-
+    
     private ArrayList<String> lines;
+    private int lastAddedIndex;
+    
+    /* (non-Javadoc)
+     * @see java.util.Observable#addObserver(java.util.Observer)
+     */
+    @Override
+    public synchronized void addObserver(Observer o) {
+        // TODO Auto-generated method stub
+        super.addObserver(o);
+    }
+
+    /* (non-Javadoc)
+     * @see java.util.Observable#notifyObservers()
+     */
+    @Override
+    public void notifyObservers() {
+        // TODO Auto-generated method stub
+        super.notifyObservers();
+    }
     
     public LineStorage() {
         lines = new ArrayList<>();
@@ -13,6 +33,8 @@ public class LineStorage extends Observable {
     
     public void addLine(String line){
         lines.add(line);
+        this.notifyObservers(lastAddedIndex++);
+        
     }
     
     public String getLine(int index){
