033de3836ec93fbd46f2eb1aca3df92a631e5a42
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-08-29 05:44:38 +0800

    Reuse utility comparator in LineStorage sort() method

--- src/com/teamfour/implicitinvocations/LineStorage.java
+++ src/com/teamfour/implicitinvocations/LineStorage.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Observable;
 
+import com.teamfour.utilities.AlphabeticalOrder;
+
 public class LineStorage extends Observable {
     private ArrayList<String> lines;
     private int lastAddedIndex;
@@ -37,7 +39,7 @@ public class LineStorage extends Observable {
     public void sort() {
         // reset currentIndex
         lastAddedIndex = 0;
-
-        Collections.sort(lines);
+        
+        Collections.sort(lines, AlphabeticalOrder.ALPHABETICAL_ORDER);
     }
 }
