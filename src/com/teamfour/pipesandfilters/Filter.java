a3dad066d10ca0af27a6c727dc174fdd0a8d5642
siro1690 <classab09@gmail.com>
2016-08-29 01:15:00 +0800

    remove Shift.java file remove text from Filter.java

--- src/com/teamfour/pipesandfilters/Filter.java
+++ src/com/teamfour/pipesandfilters/Filter.java
@@ -2,26 +2,24 @@ package com.teamfour.pipesandfilters;
 
 public abstract class Filter implements Runnable {
 	protected Pipe input_, output_;
-	private boolean started;
+	private boolean start;
 	
 	public Filter(Pipe input, Pipe output) {
 		input_ = input;
 		output_ = output;
 	}
-	
 	public void start() {
 		//System.out.println("thread has started---------------------------------------------------------------------------------------");
-
-		if (!started) {
+		if (start==false) {
 			Thread thread = new Thread(this);
 			thread.start();
 
-			started = true;
+			start = true;
 		}
 	}
 	public void stop() {
 		//System.out.println("thread has stopped*********************************************************************************************");
-		started = false;
+		start = false;
 	}
 	public void run() {
 		transform();
--- src/com/teamfour/pipesandfilters/Shift.java
+++
@@ -1,5 +0,0 @@
-package com.teamfour.pipesandfilters;
-
-public class Shift {
-
-}
