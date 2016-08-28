1eac21bb9081af88deb9cf8a9fb505b8d4d7f23d
Quek Yang Sheng Tyson <quektyson@gmail.com>
2016-08-29 06:25:37 +0800

    Add method to save output to file

--- src/com/teamfour/implicitinvocations/Output.java
+++ src/com/teamfour/implicitinvocations/Output.java
@@ -1,13 +1,38 @@
 package com.teamfour.implicitinvocations;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Output {
 
-    public static void print(LineStorage lineStorage){
+    public static void print(LineStorage lineStorage) {
         lineStorage.sort();
-        
-        while(lineStorage.hasNextLine()){
-            System.out.println(lineStorage.getLine());
+        saveOutputToFile(lineStorage);
+    }
+
+    private static void saveOutputToFile(LineStorage lineStorage) {
+        Path path = Paths.get("output.txt");
+
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+
+            while (lineStorage.hasNextLine()) {
+                writer.write(lineStorage.getLine());
+                
+                //prevent writing a new line after the last line
+                if (!lineStorage.hasNextLine()) {
+                    break;
+                }
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println("Error encounted while saving output to file.");
+            System.exit(1);
         }
+
     }
 }
-    
